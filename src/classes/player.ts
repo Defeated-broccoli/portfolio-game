f5de936083f14bfce444467cad0716638b7165ed
Defeated-broccoli <piotrkwietniewski12@gmail.com>
2024-04-28 12:28:05 +0200

    Basic movement added

--- src/classes/player.ts
+++ src/classes/player.ts
@@ -8,7 +8,11 @@ interface PlayerProps {
   depth: number
   color?: THREE.Color
   position?: THREE.Vector3
+  velocity?: THREE.Vector3
 }
+
+const playerSpeedVelocity = 3
+const playerJumpVelocity = 10
 
 export default class Player extends THREE.Mesh implements IUpdatable {
   width: number
@@ -25,12 +29,33 @@ export default class Player extends THREE.Mesh implements IUpdatable {
   //CANNON
   body: CANNON.Body
 
+  //Movement
+  keys = {
+    a: {
+      pressed: false,
+    },
+    d: {
+      pressed: false,
+    },
+    s: {
+      pressed: false,
+    },
+    w: {
+      pressed: false,
+    },
+    space: {
+      pressed: false,
+    },
+  }
+  velocity: THREE.Vector3
+
   constructor({
     width,
     height,
     depth,
     color = new THREE.Color(0xffff00),
     position = new THREE.Vector3(0, 0, 0),
+    velocity = new THREE.Vector3(0, 0, 0),
   }: PlayerProps) {
     super(
       new THREE.BoxGeometry(width, height, depth),
@@ -53,16 +78,22 @@ export default class Player extends THREE.Mesh implements IUpdatable {
     this.front = this.position.z - this.depth / 2
     this.back = this.position.z + this.depth / 2
 
+    //CANNON
     this.body = new CANNON.Body({
       mass: 1,
       position: new CANNON.Vec3(position.x, position.y, position.z),
       shape: new CANNON.Box(new CANNON.Vec3(width / 2, height / 2, depth / 2)),
     })
+
+    //Movement
+    this.velocity = velocity
+    this.setupMovement()
   }
 
   update = () => {
     this.updateCollision()
     this.updateSides()
+    this.updateVelocity()
   }
 
   updateSides = () => {
@@ -77,5 +108,69 @@ export default class Player extends THREE.Mesh implements IUpdatable {
   updateCollision = () => {
     this.position.copy(this.body.position)
     this.quaternion.copy(this.body.quaternion)
+  }
+
+  updateVelocity = () => {
+    if (this.keys.a.pressed) {
+      this.body.velocity.x = -playerSpeedVelocity
+    } else if (this.keys.d.pressed) {
+      this.body.velocity.x = playerSpeedVelocity
+    }
+
+    if (this.keys.w.pressed) {
+      this.body.velocity.z = -playerSpeedVelocity
+    } else if (this.keys.s.pressed) {
+      this.body.velocity.z = playerSpeedVelocity
+    }
+
+    if (this.keys.space.pressed) {
+      this.body.velocity.y = playerJumpVelocity
+    }
+
+    this.body.position.x += this.velocity.x
+    this.body.position.y += this.velocity.y
+    this.body.position.z += this.velocity.z
+  }
+
+  setupMovement = () => {
+    window.addEventListener('keydown', (event) => {
+      switch (event.code) {
+        case 'KeyA':
+          this.keys.a.pressed = true
+          break
+        case 'KeyD':
+          this.keys.d.pressed = true
+          break
+        case 'KeyW':
+          this.keys.w.pressed = true
+          break
+        case 'KeyS':
+          this.keys.s.pressed = true
+          break
+        case 'Space':
+          this.keys.space.pressed = true
+          break
+      }
+    })
+
+    window.addEventListener('keyup', (event) => {
+      switch (event.code) {
+        case 'KeyA':
+          this.keys.a.pressed = false
+          break
+        case 'KeyD':
+          this.keys.d.pressed = false
+          break
+        case 'KeyS':
+          this.keys.s.pressed = false
+          break
+        case 'KeyW':
+          this.keys.w.pressed = false
+          break
+        case 'Space':
+          this.keys.space.pressed = false
+          break
+      }
+    })
   }
 }
